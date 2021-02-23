90f4bea96a5fedb78b83d076bd4fba585a2b5de2
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-23 17:24:23 -0600

    Added startButton variable to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 // When page loads, have initial question box
 
 // When click start button, timer starts and new question box
+
+let startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function() {
+    console.log("hi");
+});
+
+// let questionBox = {
+//     question: "",
+//     choice1: "",
+//     choice2: "",
+//     choice3: "",
+//     choice4: "",
+//     correctAnswer: ""
+// };
