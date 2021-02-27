68a1975e651832f216ab14ca262d9db298632d8a
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-27 08:09:04 -0600

    Added const questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+const questions = [
+    {
+        question: "Which is NOT a primitive type?",
+        answers: [
+            {text: "Number", correct: false},
+            {text: "Float", correct: true},
+            {text: "String", correct: false},
+            {text: "Object", correct: false}
+        ]
+    }]
+
 // When page loads, have initial question box
 
 // When click start button, timer starts and new question box
