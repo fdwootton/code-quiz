8404a4a9965187e9666ca033da84602d9bf69f20
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-27 22:48:44 -0600

    Added styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
+// These are all the questions in the quiz
 const questionsArray = [
     {
         question: "Which is NOT a primitive type?",
-        answers: [
-            {choice: "Number", correct: false},
-            {choice: "Wire Frame", correct: true},
-            {choice: "String", correct: false},
-            {choice: "Boolean", correct: false}
-        ]
+        choice1: "Number",
+        correct: "Wire Frame",
+        choice2: "String",
+        choice3: "Boolean"
     },
     {
-        question: "API stands for: ",
-        answers: [
-            {choice: "Application Programming Interface", correct: true},
-            {choice: "Apple Program Interactives", correct: false},
-            {choice: "Angular Project IDE", correct: false},
-            {choice: "Algorithm Pair Inheritance", correct: false}
-        ]
+        question: "API stands for:",
+        correct: "Application Programming Interface",
+        choice1: "Apple Program Interactive",
+        choice2: "Angular Project IDE",
+        choice3: "Algorithm Pair Inheritance"
+    },
+    {
+        question: "JavaScript is a:",
+        choice1: "command line terminal",
+        correct: "programming language",
+        choice2: "font family",
+        choice3: "box model"
+    },
+    {
+        question: "Which Git command uploads local repository content to a remote repository?",
+        choice1: "Git Pull",
+        choice2: "Git Status",
+        correct: "Git Push",
+        choice3: "Git Checkout"
+    },
+    {
+        question: "Variables may NOT be declared with:",
+        choice1: "const",
+        choice2: "let",
+        choice3: "var",
+        correct: "if"
     }
-]
+];
 
 // When page loads, have initial question box
 
@@ -26,6 +44,8 @@ const questionsArray = [
 const startButton = document.getElementById("start-button");
 const startQuizContainer = document.getElementById("start-container");
 const questionContainer = document.getElementById("question-container");
+
+let allChoices = document.getElementsByClassName("answer-choice");
 
 let timer = document.getElementById("timer");
 timer = 0
@@ -42,6 +62,11 @@ startButton.addEventListener("click", function() {
     startQuizContainer.classList.add("hide");
     questionContainer.classList.remove("hide");
 });
+
+allChoices.addEventListener("click", function () {
+    console.log("hello");
+})
+
 
 // question container if true, backgroung green, then go to next question
 // if false then say wrong and 
